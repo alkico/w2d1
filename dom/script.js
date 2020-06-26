b9bae7b8c52b7ea945aa80fd2c1c17f6111f8575
Aleah Connley <aleahkc@gmail.com>
2020-06-26 12:41:01 +0200

    further notes

--- dom/script.js
+++ dom/script.js
@@ -1,20 +1,20 @@
 // console.log(document.body);
 
 // target an element via it's id
-const todoList = document.getElementById('todo-list');
+const todoList = document.getElementById("todo-list");
 
 // console.log(todoList);
 // returns html collection it is not an array but can be treated like one
-const allListItems = document.getElementsByTagName('li');
+const allListItems = document.getElementsByTagName("li");
 
 // console.log(allListItems[2]);
 
 // you can turn an html collection into an array
-// [...allListItems]  -  Array.from(allListItems)
+// [...allListItems] - Array.from(allListItems);
 
-const todoListItems = todoList.getElementsByTagName('li');
+const todoListItems = todoList.getElementsByTagName("li");
 
-// todoListItems[2].innerText = 'solve katas';
+// todoListItems[2].innerText = "solve katas";
 
 // console.log(todoListItems[2].innerText);
 
@@ -24,14 +24,14 @@ const todoListItems = todoList.getElementsByTagName('li');
 
 // we iterate through the list and change the inner text of each element
 // for (let i = 0; i < todoListItems.length; i++) {
-//     todoListItems[i].innerText = todoListItems[i].innerText.toUpperCase();
+//   todoListItems[i].innerText = todoListItems[i].innerText.toUpperCase();
 // }
 
 for (const item of todoListItems) {
-    item.innerText = item.innerText.toUpperCase();
+  item.innerText = item.innerText.toUpperCase();
 }
 // targets all elements where the class has `todo-container`
-const container = document.getElementsByClassName('todo-container');
+const container = document.getElementsByClassName("todo-container");
 // console.log(container[0]);
 
 // selects all elements using a css selector - returns a node list
@@ -39,92 +39,111 @@ const container = document.getElementsByClassName('todo-container');
 // console.log(container);
 
 // selects the first element that matches a querySelectorAll
-const firstItem = document.querySelector('#todo-list li');
+const firstItem = document.querySelector("#todo-list li");
 // console.log(li);
 // change style of element
-// firstItem.style.backgroundColor = 'red';
-// firstItem.style['background-color'] = 'red';
+// firstItem.style.backgroundColor = "red";
+// firstItem.style["background-color"] = "red";
 
 // access the className or the classList and change it
 // overwrites the class
-// firstItem.className = 'checked';
+// firstItem.className = "checked";
 // adds to the class / classes
-// firstItem.classList.add('checked');
+// firstItem.classList.add("checked");
 
-// firstItem.classList.remove('b');
+// firstItem.classList.remove("b");
 
-// toggles - if it is there it removes, if it is not there it adds 
-// firstItem.classList.toggle('a');
+// toggles - if it is there it removes, if it is not there it adds
+// firstItem.classList.toggle("a");
 // firstItem.classList.toggle('a');
 
 // set Attributes
-firstItem.setAttribute('id', 'first-item');
-// const attribute = firstItem.getAttribute('id');
+firstItem.setAttribute("id", "first-item");
+const attribute = firstItem.getAttribute("id");
 // console.log(attribute);
-// firstItem.removeAttribute('id');
+// firstItem.removeAttribute("id");
 
 // we can fix the broken image
-// document.querySelector('img').setAttribute(
-//     'src',
-//     'https://images.unsplash.com/photo-1564258646808-fac8e26166a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80'
-// );
-
+document
+  .querySelector("img")
+  .setAttribute(
+    "src",
+    "https://images.unsplash.com/photo-1564258646808-fac8e26166a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+  );
 
 // we wanna add a heading
 // we create it
 // // const todoList = document.getElementById('todo-list');
-const heading = document.createElement('h1');
-heading.innerText = 'My Todo List';
-const parentElement = document.querySelector('.todo-container');
+const heading = document.createElement("h1");
+heading.innerText = "My Todo List";
+const parentElement = document.querySelector(".todo-container");
 parentElement.insertBefore(heading, todoList);
 
 // let's use javascript to remove the p tag
-const paragraph = document.getElementsByTagName('p')[0];
+const paragraph = document.getElementsByTagName("p")[0];
 parentElement.removeChild(paragraph);
 
 // create a new list item
-const newListItem = document.createElement('li');
+const newListItem = document.createElement("li");
 
-// newListItem.innerText = 'watch westworld';
+// newListItem.innerText = "watch westworld";
 // you can also add html:
-newListItem.innerHTML = 'watch <strong>westworld</strong>';
+newListItem.innerHTML = "watch <strong>westworld</strong>";
+
+//alternative way to add text to an element to innerHTML
+const text = document.createTextNode("This text is created dynamically");
+
 // and add it to the dom
 todoList.appendChild(newListItem);
+todoList.appendChild(text);
 
-// i can add an event listener directly in the html
-// onclick="trigger()"
+// I can add an event listener directly in the html
+// onclick = "trigger()";
 // function trigger() {
-//     console.log('click');
+//   console.log("click");
 // }
 
-document.getElementById('add-todo').onclick = addTodo;
+document.getElementById("add-todo").onclick = addTodo;
 
 function addTodo() {
-    // test basic functionality - connection
-    // console.log('click');
-    const newTodo = document.createElement('li');
-    const input = document.querySelector('input');
-    // console.log(input.value);
-    newTodo.innerText = input.value;
-    const parent = document.getElementById('todo-list');
+  // test basic functionality - connection
+  // console.log('click');
+  const newTodo = document.createElement("li");
+  const input = document.querySelector("input");
+  // console.log(input.value);
+  newTodo.innerText = input.value;
+  const parent = document.getElementById("todo-list");
 
-    parent.appendChild(newTodo);
-    // have to add this onclick here because it is a new element and the loop
-    // on line 119 did take this in regard
-    newTodo.onclick = toggleTodo;
-    // clear the input
-    input.value = '';
+  parent.appendChild(newTodo);
+  // have to add this onclick here because it is a new element and the loop
+  // on line 119 did take this in regard
+  newTodo.onclick = toggleTodo;
+  // clear the input
+  input.value = "";
 }
 
 // add an onclick on all elements of the list
-document.querySelectorAll('#todo-list li').forEach(function (item) {
-    item.onclick = toggleTodo;
+document.querySelectorAll("#todo-list li").forEach(function (item) {
+  item.onclick = toggleTodo;
 });
 
 // we wanna add the functionality that whenever we click on a word on the todo list
 // the class 'checked' gets toggeled
 function toggleTodo(event) {
-    console.log(event);
-    // console.log(event.currentTarget.classList);
-    event.currentTarget.classList.toggle('checked');
+  console.log(event);
+  // console.log(event.currentTarget.classList);
+  event.currentTarget.classList.toggle("checked");
 }
+
+//Getting the current node object using the event object
+// The event object (sometimes referred as e) is an object inherited in every event we create.
+let theUl = document.querySelector(".list"); // gets the "ul" element
+
+//console.log(theUl.childNodes); // ==> check what's in the console
+
+theUl.childNodes.forEach((theLiTag) => {
+  theLiTag.onclick = function () {
+    console.log(theLiTag.innerHTML);
+    theUl.removeChild(theLiTag);
+  };
+});
